cde046bf3c158e5bdc80346ec3920f62b7cc0935
10e9928a <shizhao@mail.nwpu.edu.cn>
2024-07-20 14:14:09 +0800

    chore: init

--- src/config/default-setting.ts
+++ src/config/default-setting.ts
@@ -1,7 +1,7 @@
 import type { LayoutSetting } from '~@/stores/app'
 
 export default {
-  title: 'Maple 测试平台',
+  title: 'Ant Design Pro',
   logo: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
   collapsed: false,
   colorPrimary: '#1677FF',
